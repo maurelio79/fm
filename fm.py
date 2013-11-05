ba210e4269255a2ecf1461e509ba4d319ac596b2
maurelio79 <mauro.lomartire@gmail.com>
2013-11-05 22:23:55 +0100

    first commit

--- fm.py
+++ fm.py
@@ -19,7 +19,7 @@ class Fm(object):
 		Function to log module errors into log file
 		Still not complete!!
 		"""
-		logging.basicConfig(filename='nagios.log',level=logging.ERROR, format='%(asctime)s %(message)s')
+		logging.basicConfig(filename='fm_module.log',level=logging.ERROR, format='%(asctime)s %(message)s')
 		logging.debug('This message should go to the log file')
 		logging.info('So should this')
 		logging.warning('And this, too')
