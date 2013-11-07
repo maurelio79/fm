a81b5a5b392284a8f4c16f2fc781c31dfc76dea3
maurelio79 <mauro.lomartire@gmail.com>
2013-11-07 16:50:16 +0100

    added substrF function

--- fm.py
+++ fm.py
@@ -1,8 +1,27 @@
 """
 fm.py - manage operation on text file
+
+This module helps programmer to quickly manage text file.
+It's possible read, write, append, prepend, find & replace any text to any file.
+
+logF function will log module error in fm_module.log.
+
+To use this module, import this module and create class object in your script:
+
+from fm import Fm
+fm = Fm()
+
+then call function inside this module with prefix fm.
+For example to call the read function just write:
+
+fm.readF()
+
+The __init__ function of class Fm will import
+sys, os, string, commands
+so you don't need to import them in your script.
 """
-#import sys, os, string, commands
-import sys, string, logging
+
+import sys, string, logging, fileinput
 
 class Fm(object):
 	"""FileManager Module
@@ -36,6 +55,9 @@ class Fm(object):
 		It's usefull pass the param 'list' if you don't want just print
 		the file content, but you want put the file content in a variable
 		as a list.
+		
+		Example:
+			fm.ReadF("file.txt", param=['list'])
 		"""
 		try:
 			with open(data, 'r') as f:
@@ -49,7 +71,6 @@ class Fm(object):
 					return self.text
 			f.close()
 		except IOError:
-			#print "Error ", sys.exc_info()
 			self.logF(sys.exc_info()[1])
 
 	def writeF(self, data, mode, text):
@@ -66,7 +87,6 @@ class Fm(object):
 				f.write(text)
 			f.close()
 		except IOError:
-			#print "Error ", sys.exc_info()
 			self.logF(sys.exc_info()[1])
 
 	def prependF(self, data, text):
@@ -76,6 +96,9 @@ class Fm(object):
 		Required arguments:
 		- data -> file name to write
 		- text -> text to prepend
+
+		Example:
+			fm.prependF("file.txt", "This text will be prepend to file")
 		"""
 		try:
 			with open(data, 'r') as f_old:
@@ -85,10 +108,34 @@ class Fm(object):
 						f.write(text + "\n" + text_old)
 					f.close()
 				except IOError:
-					#print "Error ", sys.exc_info()
 					self.logF(sys.exc_info()[1])
 			f_old.close()
 		except IOError:
-			#print "Error ", sys.exc_info()
 			self.logF(sys.exc_info()[1])
+
+	def substrF(self, data, dic):
+		"""substrF function
+		Function to find and replace text in file.
+
+		Required arguments:
+		- data -> file name to modify
+		- dic -> string to find and replacer string.
+
+		You nned to pass dic as python dictionary and you can replace as many string as you want.
+
+		Example:
+			dic = {'-':'*', 'old_text':'new_text'}
+			fm.substrF("file.txt", dic)
+
+		In file.txt every occurence of '-' will be substitute with '*'
+		and every occurence of 'old_text will be substitute with 'new_text'
+		"""
+		text = self.readF(data)
+
+		for i, j in dic.iteritems():
+			text = text.replace(i, j)
+		self.writeF(data, "w", text)			
+
+
+
 
